37e2571bb54ed3957f326024ea3fe8c597c32f50
unknown <afia.nasrin3e@gmail.com>
2022-10-12 06:22:45 +0600

    errorpage are compleate

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { createContext } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Navbar from "./Navbar";
+const QuizContext = createContext([]);
 const Root = () => {
   return (
     <div>
